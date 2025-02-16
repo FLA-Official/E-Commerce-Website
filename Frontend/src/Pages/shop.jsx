b6c7e6ef0380ff2ef101dd2bf0ae2f2c1db11849
FLA-Official <flapon.official@gmail.com>
2025-02-17 03:24:59 +0600

    Starting With Frontend- NavBar, Hero

---
+++ Frontend/src/Pages/shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero.jsx';
+
+const shop = () => {
+  return (
+    <div>
+        <Hero/>
+
+    </div>
+  )
+}
+
+export default shop;
