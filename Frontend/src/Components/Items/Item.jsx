37746ec51b708b27dbf8b9fb0ab18f64a00a4a85
FLA-Official <flapon.official@gmail.com>
2025-02-18 02:09:59 +0600

    update - Item+product

---
+++ Frontend/src/Components/Items/Item.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Item.css'
+
+ const Item = (props) => {
+  return (
+    <div className='item'> 
+    <img src={props.image} alt="" />
+    <p>{props.name}</p>
+    <div className='item-prices'></div>
+        <div className='item-price-new'>
+        ${props.new_price}
+        </div>
+        <div className='item-price-old'>
+        {props.old_price}
+        </div>
+
+    </div>
+  )
+}
+Item.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  new_price: PropTypes.number.isRequired,
+  old_price: PropTypes.number.isRequired,
+};
+
+export default Item;
