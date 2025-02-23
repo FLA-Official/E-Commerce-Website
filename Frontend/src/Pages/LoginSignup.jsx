eb59b28855a64c986d9fafc9fc5e99a88598f3ba
FLA-Official <flapon.official@gmail.com>
2025-02-24 02:41:32 +0600

    Update Shopcategory, LoginSignUp

--- Frontend/src/Pages/LoginSignup.jsx
+++ Frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Adress" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          {" "}
+          Already have an account? <span>LogIn here</span>{" "}
+        </p>
+
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>
+            {" "}
+            By Continuing I agree to the terms of use and privacy policies.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginSignup;
--- Frontend/src/Pages/ShopCategory.jsx
+++ Frontend/src/Pages/ShopCategory.jsx
@@ -1,20 +1,54 @@
-import React, { useContext } from 'react'
-import PropTypes from 'prop-types';
-import './CSS/ShopCategory.css'
-import { ShopContext } from '../Context/ShopContext'
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import Item from "../Components/Items/Item";
 
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
-  return (
-    <div className='shop-category'>
-      <img src={props.banner} alt="" />
-    </div>
+  const { all_product } = useContext(ShopContext);
 
-  )
-}
+  if (!all_product || all_product.length === 0) {
+    return <p>Loading products...</p>;
+  }
+
+  return (
+    <div className="shop-category">
+      <img className="shopcategory-banner" src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span>
+          out of 36 product
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else return null;
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
+      </div>
+    </div>
+  );
+};
 ShopCategory.propTypes = {
   banner: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default ShopCategory;
-
